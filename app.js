3f09d65167388a779bb7600da4426412fa44866b
shivmohan-pal <shivmohan7895@gmail.com>
2022-05-26 13:33:03 +0530

    second requirement

--- app.js
+++ app.js
@@ -11,3 +11,34 @@ var teamList = [
     {name: 'CSK',points: 08, last5Results:[0,0,1,0,1]},
     {name: 'MI',points: 06, last5Results:[0,1,0,1,1]}
 ];
+
+
+class Team{
+    constructor(teamName,points,results){
+        this.name = teamName;
+        this.points = points;
+        this.results = results;
+        // this.results = typeof results === 'string' || 'number' ? [results] : [...results];
+    }
+
+   has2ConsecutiveLosses(){//function of 2 consecutive losses
+       var flag=0;
+     for(let i=0;i<this.results.length;i++){
+        flag=(!this.results[i] || this.results[i]==='loss') ? flag+1 : 0;
+        if(flag===2){return true;}
+     }
+      return false;
+   }
+}
+
+function TeamsWithConsecutiveLossOrWin(teamsList){// paramter list of team with paritcular data
+    var loserTeams = [];
+    for(let i=0;i<teamsList.length;i++){
+     if(new Team(teamsList[i].name,teamsList[i].points,teamsList[i].last5Results).has2ConsecutiveLosses()) // no parameter for 2 consecutive losses
+         loserTeams.push(teamsList[i])
+    }
+    return loserTeams;
+ }
+ 
+ 
+ console.log('Filtered Teams : ' ,TeamsWithConsecutiveLossOrWin(teamList));// output in console
