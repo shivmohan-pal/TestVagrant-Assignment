ab8514494f58b433230ab5101b0e6c6d25e8edeb
shivmohan-pal <shivmohan7895@gmail.com>
2022-05-26 13:23:58 +0530

    first requirement

---
+++ app.js
@@ -0,0 +1,13 @@
+// given teams data
+var teamList = [
+    {name: 'GT',points: 20, last5Results:[1,1,0,0,1]},// win is donoted as 1 and loss as 0
+    {name: 'LSG',points: 18, last5Results:[1,0,0,1,1]},
+    {name: 'RR',points: 16, last5Results:[1,0,1,0,0]},
+    {name: 'DC',points: 14, last5Results:[1,1,0,1,0]},
+    {name: 'RCB',points: 14, last5Results:[0,1,1,0,0]},
+    {name: 'KKR',points: 12, last5Results:[0,1,1,0,1]},
+    {name: 'PBKS',points: 12, last5Results:[0,1,0,1,0]},
+    {name: 'SRH',points: 12, last5Results:[1,0,0,0,0]},
+    {name: 'CSK',points: 08, last5Results:[0,0,1,0,1]},
+    {name: 'MI',points: 06, last5Results:[0,1,0,1,1]}
+];
